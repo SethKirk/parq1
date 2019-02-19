d30b7fcf7862749ab383d6b0af380103bb222caa
Seth Kirksey <sethkirksey2@gmail.com>
2019-02-19 02:46:44 -0500

    Initial Commit

--- Parq/ViewController.swift
+++ Parq/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "Hello World!"
+        textField.becomeFirstResponder()
     }
-
+    /**
+     Adds two numbers together and returns the sum.
+     - parameters:
+     - num1: The first number
+     - num2: The second number
+     - returns: The sum of num1 and num2.
+     */
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+    @IBAction func buttonWasTapped(_ sender: Any) {
+        label.text = "Sup Bitch \(textField.text!)"
+        textField.resignFirstResponder()
+    }
+    
 
 }
 
